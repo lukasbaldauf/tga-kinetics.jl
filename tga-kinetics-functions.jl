fc4c79c29a606f4c0ed0c0ea32ad9f28223facdb
Lukas Baldauf <74910029+lukasbaldauf@users.noreply.github.com>
2021-08-23 18:09:26 +0200

    Add files via upload

--- tga-kinetics-functions.jl
+++ tga-kinetics-functions.jl
@@ -1,31 +1,24 @@
-function dydt2(T::Float64,y::Float64,pj::Array{Float64,1},j::Int64,m::Int64) ::Float64
-    #if y < 0.0
-    #    return 0.0
-    #else
-        return @inbounds -(10^pj[j])*exp(-pj[j+m]/(8.3145*(T)))*y^pj[j+2*m]
-    #end
-end
-
 @inline function dydt(T::Float64,y::Float64,pj::Array{Float64,2},j::Int64,m::Int64) ::Float64
+    """ This function returns the equation governing the kinetics of the systems
+    considered.
+    """
     if y < 0.0
         return 0.0
     else
+        #return @inbounds -10^(pj[j]+pj[j+4*m]*T+pj[j+5*m]*T^2)*exp(-(pj[j+m])/(8.3145*(T)))*y^pj[j+2*m]
         return @inbounds -10^(pj[j])*exp(-(pj[j+m])/(8.3145*(T)))*y^pj[j+2*m]
-        #return @inbounds -10^(pj[j]+pj[j+4*m]*T)*exp(-(pj[j+m]+pj[j+5*m]*T)/(8.3145*(T)))*y^pj[j+2*m]
-    end
-end
-
-
-function dydt(T::Float64,y::Float64,pj::Array{Float64,1}) ::Float64
-    if y < 0.0
-        return 0.0
-    else
-        return @inbounds -(10^pj[1])*exp(-pj[2]/(8.3145*(T)))*y^pj[3]
     end
 end
 
 
 function heun!(dt,y0,n,m,temp,dx,y,pe) :: Float64
+    """ Integrate along temperature (instead of the usual
+    time) which can vary non-linearly using Heun's method (a predictor-corrector
+    method). At the same time, subtract the calculated conversion rate and
+    from the experimental conversion rate to directly calculate the deviation
+    from the two, which is minimized by the optimization algorithm to find the
+    best kinetic parameters.
+    """
     @views y[1,:]=y0
     OF ::Float64 = 0.0
     max_rate = 0.0
@@ -50,38 +43,16 @@ function heun!(dt,y0,n,m,temp,dx,y,pe) :: Float64
     return OF/(max_rate^2*n)
 end
 
-
-function heun2!(dt,y0,n,m,temp,dx,y,
-    f1::Float64,f2::Float64,p)
-    @views y[1,:]=y0
-    OF ::Float64 = 0.0
-    max_rate = 0.0
-    for i = 1:n-1
-        # sum partial components
-        DY = 0.0
-        T1 = temp[i]
-        T2 = temp[i+1]
-        dxi = dx[i]
-        for j = 1:m
-            y1  = y[i,j]
-                f1=dydt2(T1, y1, p, j, m)
-                f2=dydt2(T1, y1 + dt * f1, p, j, m)
-                y[i+1,j] = y1 + dt * ( f1+f2) / 2.0
-                DY += -1/p[j+3*m]*dydt2(T1,y1,p,j,m)
-        end
-        if dxi > max_rate
-            max_rate = dxi
-        end
-        OF += (dxi-DY)^2
-    end
-    return OF/(max_rate^2*n)
-end
-
 function get_maxrate(dxi::Float64,max_rate::Float64) :: Float64
+    # Not used
         return max(max_rate,dxi)
 end
 
 function minimize!(p,pall,indmap,pmat,m,n_d,np,T,DX,N,Y,DT,Y0)
+    """
+    The objective function to be minimized by some
+    minimization algorithm.
+    """
     OF  = 0.0
     construct_pmat!(p,pall,indmap,pmat,n_d,m,np)
     for i = 1:n_d
@@ -91,14 +62,6 @@ function minimize!(p,pall,indmap,pmat,m,n_d,np,T,DX,N,Y,DT,Y0)
     return OF
 end
 
-function minimize2!(p,m,n_d,T,DX,N,Y,DT,Y0)
-    f1 = 0.0 ; f2 = 0.0 ; f3 = 0.0 ; f4 = 0.0
-    OF = 0
-    for i = 1:n_d
-        OF += heun2!(DT[i],Y0[i],N[i],m,T[i],DX[i],Y[i],f1,f2,p)
-    end
-    return OF
-end
 
 function rk4!(dt,y0,n,m,temp,dx,y,
     f1::Float64,f2::Float64,f3::Float64,f4::Float64,p)
@@ -123,6 +86,8 @@ function rk4!(dt,y0,n,m,temp,dx,y,
 end
 
 function plot_res()
+    """ Crude plot of the results.
+    """
         plt=1
         for i = 1:n_d
                 plt=plot_result(plt,i,X[i],DX[i],N[i],m,res.minimizer,
@@ -131,9 +96,26 @@ function plot_res()
 end
 
 function mapper(pmat,tpe)
-        # Gives rise to two arrays:
-                # par which is for optimization
-                # pfxd which is the array for fixed values
+        """ construct a 1D array from a 2D array of parameter values, e.g.
+
+        PMAT = [[A1, E1, n1, c1],
+                [A2, E2, n2, c2]]
+
+        and extract the parameter values that are to be passed to
+        optimization algorithm. Doing that, we can account for
+        some parameters that set as constants during optimization,
+        some parameters that are common for severals experiments (i.e.
+        1 parameters describes the kinetics of multiple experiments) or
+        that some parameters are individual for one experiments (i.e. 1 parameter
+        is used to describe the kinetics of 1 experiment). These are described
+        by the TPE array, where 1 = common parameters, 2 = individual parameters
+        and 3 = fixed parameters.
+
+        Outputs:
+                par - containing the parameters to be optimized
+                pfxd - containing the fixed parameter values
+                indmap - indices accesing the optimization element values
+        """
         # 1D array to store values to optimize which is taken in
         # by optimizer
         pvar = Array{Float64,1}()
@@ -184,58 +166,20 @@ function mapper(pmat,tpe)
 end
 
 
-function construct_pmat__!(p, pall, indmap, pmat)
-        """
-        Add the optimization elements contained in p
-        to the array containing the optimization elements
-        in addition to the fixed values.
 
-        p       : array, usually passed from optimization algorithm
-        pall    : array, containing all values of p and the additional fixed values
-        indmap  : the mapping created from the mapper() function
-        pmat    : the output matrix containing the optimization parameters
-                and the fixed parameters.
-        """
-        pall[1:length(p)] .= p
-        for (i,val) in enumerate(indmap)
-                pmat[i]=pall[val]
-        end
-end
-
-function construct_pmat__!(p, pall, indmap, pmat,
-                         n_exp, n_comp, n_part)
-        """
-        Add the optimization elements contained in p
-        to the array containing the optimization elements
-        in addition to the fixed values.
-
-        p       : array, usually passed from optimization algorithm
-        pall    : array, containing all values of p and the additional fixed values
-        indmap  : the mapping created from the mapper() function
-        pmat    : the output matrix containing the optimization parameters
-                and the fixed parameters.
-        """
-        for e in n_exp
-            for c in n_comp
-                for p in n_part
-                    pmat[e][c][p]=pall[indmap[e,c,p]]
-                end
-            end
-        end
-end
 
 function construct_pmat!(p, pall, indmap, pmat,
                          n_exp, n_comp, n_part)
         """
         Add the optimization elements contained in p
-        to the array containing the optimization elements
-        in addition to the fixed values.
+        to an array of fixed format resulting in an array
+        containing the optimization elements in addition to the fixed values.
 
         p       : array, usually passed from optimization algorithm
-        pall    : array, containing all values of p and the additional fixed values
+        pall    : array, containing all values of p and the fixed parameter values
         indmap  : the mapping created from the mapper() function
         pmat    : the output matrix containing the optimization parameters
-                and the fixed parameters.
+                  and the fixed parameters.
         """
         pall[1:length(p)] .= p
         for e in 1:n_exp
@@ -249,4 +193,74 @@ function toSVector(pmat)
     ne,nc,np=size(pmat)
     pmat = [ pmat[e,:,:] for e in 1:ne]
     return pmat
+end
+
+function savetxt(fname)
+        """ Save 'simulated' TGA data."""
+
+        for e in 1:ne
+                f = open(fname*"$e.txt", "w")
+                write(f,"# Simulated TGA data with $nc partial components.\n")
+                write(f, "# Format: component [A, E, n, c, ...(extra_params)... ] \n")
+                out = 0.0*Y[e][:,1]
+                for c in 1:nc
+                        write(f, "# comp. $c " * string(round.(pmat[e][c,:],digits=4))*"\n")
+                        out .= out .+ Y[e][:,c]*pmat[e][c,end]
+                end
+                # normalized mass to "mg"
+                out*=1.43
+                # write to file
+                write(f,"#--------------------------------------------------------------\n")
+                write(f, "# time (min)   Temp (°C)    mass (mg) \n")
+                writedlm(f,[X[e][:,1] X[e][:,2] out], "  ")
+
+        end
+end
+
+function load_data(filenames, smoothing_factors, normalizing_temperature,
+        cut, cols, skiprows)
+        """ Load, cut, normalize and calculate the derivative of the raw data.
+
+                filenames : Load the time, temperature and mass data from these
+                            files.
+
+                smoothing_factors : the smoothing factors used in the smoothing
+                                    splines for calculating deriavtive.
+
+                normalizing_tempearture : Normalize the mass data using the
+                                          masses at these temperatures.
+
+                cut : consider only data points in the range cut[min_T, max_T]
+
+                cols : the time, temp and mass data are containted in cols[1],
+                       cols[2] and cols[3], respectively.
+
+                skiprows : do not read this amount of rows from the beginning
+                           of the file, which are e.g. comments and other
+                           non-numerical values.
+        """
+        # Array for mass conversion
+        X = []
+        # array for temperatures
+        T = Array{Array{Float64,1},1}()
+        # array for mass conversion rate
+        DX = Array{Array{Float64,1},1}()
+        for (i,file) in enumerate(filenames)
+                x = DelimitedFiles.readdlm(file,comments=true,comment_char='#',skipstart=skiprows)
+                normalizing_mass = x[:,cols[3]][x[:,cols[2]] .> normalizing_temperature[i]][1]
+                #x = x[1:floor(Int,length(x[:,1])/500):end,:]
+                println(normalizing_mass)
+                ndx = ( cut[i][2] .> x[:,cols[2]].> cut[i][1])
+                x = Matrix([x[:,cols[1]][ndx] x[:,cols[2]][ndx] x[:,cols[3]][ndx]/normalizing_mass])
+                #x = Matrix([x[:,1][ndx]*60 x[:,2][ndx] x[:,3][ndx]])
+                push!(X,x)
+                # °C to kelving
+                push!(T,x[:,2].+273.15)
+                spl = Spline1D(x[:,1],x[:,3],s=smoothing_factors[i])
+                dx = -derivative(spl,x[:,1])
+                dx = dx
+                #plot(x[:,1],dx)
+                push!(DX,dx)
+        end
+        return X,T,DX
 end
--- tga-kinetics.jl
+++ tga-kinetics.jl
@@ -2,81 +2,84 @@ using Plots
 using Printf
 using DelimitedFiles
 using Optim
-using BenchmarkTools
-using Dierckx
+using Dierckx # splines
 using StaticArrays
-using Interpolations
 include("tga-kinetics-functions.jl")
 
-
 # load data
-
 filenames = ["test_data1.txt","test_data2.txt","test_data3.txt"]
 
-# Array for mass conversion
-X = []
-# array for temperatures
-T = Array{Array{Float64,1},1}()
-# array for mass conversion rate
-DX = Array{Array{Float64,1},1}()
 # smoothing factors for smoothing spline
-s = [1e-4, 1e-4, 1e-4]
-norms=zeros(3)
-# consider only experimental points in the range [low_T, high_T]
-cut = [ [400,1200],[400,1200],[400,1200]]
-for (i,file) in enumerate(filenames)
-        x = DelimitedFiles.readdlm(file,comments=true,comment_char='#',skipstart=50)
-        mass_norm = x[:,3][x[:,1] .< 150][1]
-        #mass_norm[i]=mass_norm
-        #x = x[1:floor(Int,length(x[:,1])/500):end,:]
-        ndx = ( cut[i][2] .> x[:,2].> cut[i][1])
-        x = Matrix([x[:,1][ndx]*60 x[:,2][ndx] x[:,3][ndx]])
-        #x = Matrix([x[:,1][ndx]*60 x[:,2][ndx] x[:,3][ndx]])
-        push!(X,x)
-        push!(T,x[:,2].+273.15)
-        spl = Spline1D(x[:,1],x[:,3],s=s[i])
-        dx = -derivative(spl,x[:,1])
-        dx = dx
-        #plot(x[:,1],dx)
-        push!(DX,dx)
-end
-ne=length(filenames) # number of experiments
-nc=1 # number of partial components
-np=4 # number of parameters in model: A E n c
+smoothing_factors = [5e-6, 1e-5, 5e-6]
+# consider only experimental points in the range cut[low_T, high_T]
+cut = [ [200,1200],[200,1200],[200,1200]]
+# array containg the normalizing mass
+normalizing_temperature=[cut_i[1] for cut_i in cut]
+# the columns containing the time, temperature and mass data in the files, respectivley
+cols = [1,2,3]
+# Do not read the this amount of the first rows of the initial file.
+# could be e.g. comments in the data files.
+skiprows = 0
+# number of experiments
+ne=length(filenames)
+# number of partial components
+nc=2
+# number of parameters in model: A E n c = 4 parameters.
+# Remember to change this when using a different model
+np=4
 
+# load the data
+# X = mass data
+# T = temperature
+# DX = negative of the mass loss rate
+X,T,DX = load_data(filenames, smoothing_factors, normalizing_temperature,
+                cut, cols, skiprows)
+# arrays to hold kinetic parameter values
 parmtype = zeros(Int64,ne,nc,np)
 pmat = zeros(Float64,ne,nc,np)
 recon = zeros(Float64,ne,nc,np)
-# initial parameters
-pmat[1,:,:]=[8 200_000 1 0.2]
-pmat[2,:,:]=[8 200_000 1 0.2]
-pmat[3,:,:]=[8 200_000 1 0.2]
-#pmat[2,:,:]=[-112.18844 -979604.402715 0.843048 2.026574 0.057221 -0.288351]
-#pmat[3,:,:]=[-38.395383 -249473.833356 0.817539 1.874923 0.020734 -16.821003]
+# Pre-allocate memory for output arrays
+# number of data points in each experiment
+N = [length(DX[i]) for i=1:ne]
+# calculated mass conversion
+Y = [zeros((N[i],nc)) for i = 1:ne]
+# Time step in experiments
+DT = [X[i][2,1]-X[i][1,1] for i = 1:ne]
+# initial mass values (initial value of the integration)
+Y0 = [ones(nc) for i = 1:ne]
+
+# initial parameter values
+pmat[1,:,:]=[10 200_000 1 0.4 ; 8 190_000 1 0.6]
+pmat[2,:,:]=[10 200_000 1 0.4 ; 8 190_000 1 0.6]
+pmat[3,:,:]=[10 200_000 1 0.4 ; 8 190_000 1 0.6]
 # 1 = common parameters
 # 2 = individual parameters
 # 3 = fixed parameter
-parmtype[1,:,:]=[ 2 1 1 1]
-parmtype[2,:,:]=[ 2 1 1 1]
-parmtype[3,:,:]=[ 2 1 1 1]
-#parmtype[2,:,:]=[ 3 3 3 3 3 3]
-#parmtype[3,:,:]=[ 1 1 1 1 1 1]
-
-N = [length(DX[i]) for i=1:ne]
-Y = [zeros((N[i],nc)) for i = 1:ne]
-DT = [X[i][2,1]-X[i][1,1] for i = 1:ne]
-Y0 = [ones(nc) for i = 1:ne]
+parmtype[1,:,:]=[ 1 1 1 1; 1 1 3 1]
+parmtype[2,:,:]=[ 1 1 1 1; 1 1 3 1]
+parmtype[3,:,:]=[ 1 1 1 1; 1 1 3 1]
 
 
+
+
+# construct the mapping, i.e. determine the array that is to be passed to the
+# optimizer, taking into accound that some parameters are common, individual or
+# constat.
 p0,pall,indmap=mapper(pmat,parmtype)
+# convert to SVector
 pmat=toSVector(pmat)
 construct_pmat!(p0,pall,indmap,pmat,ne,nc,np)
 
-#p0=res.minimizer
+# Use a closure to call
+# minimize!(minimize!(p,pall,indmap,pmat,nc,ne,np,T,DX,N,Y,DT,Y0))
+# as minimize!(p) and optimize it.
 res=optimize(p->minimize!(p,pall,indmap,pmat,nc,ne,np,T,DX,N,Y,DT,Y0),
         p0,
         NelderMead(),
-            Optim.Options(iterations=4000))
+        Optim.Options(iterations=4000))
+
+
+# Print and plot
 println()
 @printf "minimum: %7.5e  iters: %i \n\n" res.minimum res.iterations
 
@@ -84,15 +87,15 @@ for e = 1:ne
         #pmat[e]=pmat[1]
         heun!(DT[e],Y0[e],N[e],nc,T[e],DX[e],Y[e],pmat[e])
         plt=plot()
-        plot!(plt,X[e][:,1],DX[e],label="experimental")
+        plot!(plt,X[e][:,1],DX[e],label="experimental rate")
         dsum = zeros(length(DX[e]))
         for c = 1:nc
                 dc = [-pmat[e][c+3*nc]*dydt(T1,y1,pmat[e],c,nc) for (T1,y1) in zip(T[e],Y[e][:,c])]
                 dsum .= dsum .+ dc
-                #plot!(plt,X[e][:,1],dc)
+                plot!(plt,X[e][:,1],dc,label="component $c")
         end
-        plot!(plt,X[e][:,1],dsum,title="reldev = $(round(sqrt(sum(abs2, (dsum .- DX[e]))/(maximum(DX[e])^2*N[e]))*100,digits=2)) %",
-        label="calculated",ylabel="conversion rate")
+        plot!(plt,X[e][:,1],dsum,title="Deviation = $(round(sqrt(sum(abs2, (dsum .- DX[e]))/(maximum(DX[e])^2*N[e]))*100,digits=2)) %",
+        label="sum calculated",ylabel="- mass loss rate",xlabel="time (min)")
 
         @printf "pmat[%s,:,:]=%s\n"  e round.(pmat[e],digits=6)
         display(plt)
